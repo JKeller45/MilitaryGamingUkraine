dc83f290fabdc8d05081c1a95ad327220d87d34f
jkeller45 <jkeller45@gatech.edu>
2025-03-25 15:07:46 -0400

    final changes

--- analyze.py
+++ analyze.py
@@ -1,7 +1,7 @@
 import data_analysis as da
 import polars as pl
 
-file_name = 'ukraine_[0.25, 0.25, 0.25, 0.25]_russia_[0.25, 0.25, 0.25, 0.25]_aid_0.24_sanctions_0.14'
+file_name = input("Enter the csv file name (excluding the .csv extension): ")
 data = pl.read_csv(f"data/{file_name}.csv", glob=False)
 
 da.plot_monte_carlo_histograms(data['lengths'], data['winners'], data['reasons'])
