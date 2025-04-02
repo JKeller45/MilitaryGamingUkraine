66e2ba6f66fc412552135a74d5be23ff602e3474
jkeller45 <jkeller45@gatech.edu>
2025-04-02 13:15:57 -0400

    random sims

--- run_full_simulation_space.py
+++ run_full_simulation_space.py
@@ -1,29 +1,55 @@
 from monte_carlo_simulation import run_monte_carlo_simulation
 import gc
+import numpy as np
 
 def run_full_simulation_space(num_simulations: int):
     international_interference_scenarios: list = []
     investment_policies_scenarios: list = []
 
-    for military_tech_percentage in [.15, .25, .35]:
-        for industrial_tech_percentage in [.15, .25, .35]:
-            for military_industrial_percentage in [.15, .25, .35]:
-                for civilian_industrial_percentage in [.15, .25, .35]:
-                    if military_tech_percentage + industrial_tech_percentage + military_industrial_percentage + civilian_industrial_percentage == 1:
-                        investment_policies_scenarios.append({
-                            'russia': [military_tech_percentage, industrial_tech_percentage, military_industrial_percentage, civilian_industrial_percentage],
-                            'ukraine': [military_tech_percentage, industrial_tech_percentage, military_industrial_percentage, civilian_industrial_percentage]
-                        })
+    num_sims = 150
 
-    for sanctions_russia in [.07, .14, .21]:
-        for foreign_aid_ukraine in [.12, .24, .36]:
+    # for military_tech_percentage in [.15, .25, .35]:
+    #     for industrial_tech_percentage in [.15, .25, .35]:
+    #         for military_industrial_percentage in [.15, .25, .35]:
+    #             for civilian_industrial_percentage in [.15, .25, .35]:
+    for _ in range(num_sims):
+        rand_ints = [round(np.random.uniform(.05, 1), 2) for _ in range(3)]
+        rand_ints = sorted(rand_ints)
+
+        military_tech_percentage = rand_ints[0]
+        industrial_tech_percentage = rand_ints[1] - rand_ints[0]
+        military_industrial_percentage = rand_ints[2] - rand_ints[1]
+        civilian_industrial_percentage = 1 - rand_ints[2]
+
+        ukr = [military_tech_percentage, industrial_tech_percentage, military_industrial_percentage, civilian_industrial_percentage]
+
+        rand_ints = [round(np.random.uniform(.05, 1), 2) for _ in range(3)]
+        rand_ints = sorted(rand_ints)
+
+        military_tech_percentage = rand_ints[0]
+        industrial_tech_percentage = rand_ints[1] - rand_ints[0]
+        military_industrial_percentage = rand_ints[2] - rand_ints[1]
+        civilian_industrial_percentage = 1 - rand_ints[2]
+
+        russia = [military_tech_percentage, industrial_tech_percentage, military_industrial_percentage, civilian_industrial_percentage]
+
+        if sum(ukr) == 1 and sum(russia) == 1:
+            investment_policies_scenarios.append({
+                'russia': russia,
+                'ukraine': ukr
+            })
+
+    # for sanctions_russia in [.07, .14, .21]:
+    #     for foreign_aid_ukraine in [.12, .24, .36]:
             international_interference_scenarios.append({
-                'sanctions_russia': sanctions_russia,
-                'foreign_aid_ukraine': foreign_aid_ukraine,
+                'sanctions_russia': round(np.random.uniform(0, .25), 2),
+                'foreign_aid_ukraine':  round(np.random.uniform(0, .5), 2),
                 'foreign_aid_russia': 0,
                 'sanctions_ukraine': 0,
             })
 
+    # print(investment_policies_scenarios)
+    # print(international_interference_scenarios)
     for investment in investment_policies_scenarios:
         for interference in international_interference_scenarios:
             run_monte_carlo_simulation(num_simulations, interference, investment, export=True)
